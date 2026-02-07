c884446048101f714a603f563fae3ce6483e0c4d
DanielDanielsson <danielssondaniel94@gmail.com>
2026-02-07 12:14:49 +0100

    chore: update sha256 for v1.0.0 release

--- Casks/lorema.rb
+++ Casks/lorema.rb
@@ -1,6 +1,6 @@
 cask "lorema" do
   version "1.0.0"
-  sha256 "PLACEHOLDER_UPDATE_AFTER_FIRST_RELEASE"
+  sha256 "e37be5b09dcb0d75830b73aa032a7cc1be4df64556c5fdb88ed5f083ef0278f8"
 
   url "https://github.com/DanielDanielsson/lorema-app/releases/download/v#{version}/lorema-macos.zip"
   name "Lorema"
